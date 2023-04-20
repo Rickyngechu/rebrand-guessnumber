a66efe30dd2bf02b080b19321c15d10febb1f063
Rickyngechu <ekngechu003@gmail.com>
2023-04-21 01:10:55 +0300

    Modifications

--- script.js
+++ script.js
@@ -1,60 +1,66 @@
 'use strict';
-const inputArea = document.querySelector('.input-text');
+let inputArea = document.querySelector('.input-text');
 const btnCheck = document.querySelector('.btn-check');
 const header = document.querySelector('.header');
 const mainSect = document.querySelector('.section__main');
 const guessText = document.querySelector('.guess-start');
+const btnAgain = document.querySelector('.again-btn');
 
 let correctNum = document.querySelector('.answer-txt');
 
-let score = document.querySelector('.scoreV');
+let scoreV = document.querySelector('.scoreV');
+// let scoreValue = (score.innerHTML = 20);
 let highScore = document.querySelector('.highestV');
-
-// const update = function () {
-
-// };
-// update();
-// update();
 
 //STEP 1) Generate random number which is between 1 and 20
 //Math.random generates number which is between 0.0 and 1.0
 //We are adding one in order the range to be from 1 to 20
 //If we had not added the 1 then range would have been 0 to 19
-
-//STEP 2)We are getting the input value when a user inputs the number
-//Since the user's input will be string we must covert it into number
+const random = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
 
 btnCheck.addEventListener('click', function (e) {
   e.preventDefault();
 
-  //   We generate the random number which is between 1 and 20
-  const random = Math.trunc(Math.random() * 20 + 1);
-  console.log(random);
-
-  //For testing purposes(remember to remove this)
-  //   const random = 12;
-  //Convert the input value to be a number
+  //Getting the input value and converting it into a number
   const inputValue = +inputArea.value;
 
-  //We check if the input value is too high or too low
-  if (inputValue > random) {
-    guessText.innerHTML = '📈 Too high';
-  }
-  if (inputValue < random) {
-    guessText.innerHTML = '📉 Too low';
-  }
-  const checkInputNumber = function (input) {
-    if (random === input) {
-      header.style.backgroundColor = 'green';
-      mainSect.style.backgroundColor = 'green';
+  if (!inputValue) {
+    guessText.textContent = '😶 No number entered!';
+  } else if (inputValue === random) {
+    header.style.backgroundColor = '#30ac44';
+    mainSect.style.backgroundColor = '#30ac44';
 
-      highScore.innerHTML = input;
-      correctNum.innerHTML = input;
-    } else random !== input;
-    //If the guess is wrong decrease the number by one
-    score.innerHTML--;
+    guessText.textContent = '🥳 Genious,correct guess!';
+    highScore.textContent = scoreV.textContent;
+    correctNum.textContent = random;
+  } else if (inputValue < random) {
+    if (score > 1) {
+      guessText.textContent = '📉 Value too low';
+      score--;
+      scoreV.textContent = score;
+    } else {
+      guessText.textContent = '😥 You have lost.Try again!';
+    }
+  } else if (inputValue > random) {
+    if (score > 1) {
+      guessText.textContent = '📈 Value too high';
+      score--;
+      scoreV.textContent = score;
+    } else {
+      guessText.textContent = '😥 You have lost.Try again!';
+    }
+  }
+});
 
-    console.log(input);
-  };
-  checkInputNumber(inputValue);
+btnAgain.addEventListener('click', function (e) {
+  e.preventDefault();
+  guessText.textContent = 'Start guessing...';
+  inputArea.value = '';
+  correctNum.textContent = '?';
+  header.style.backgroundColor = '#fce9e9';
+  mainSect.style.backgroundColor = '#fce9e9';
+
+  highScore.textContent = scoreV.textContent;
+  scoreV.textContent = 20;
 });
